2f4ac86a46b22e7f71ad54b009ec07c5daf52e7a
samc995 <Sam995@gmail.com>
2023-03-05 21:16:04 -0800

    updated push

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
-var startPage = document.querySelector(".start-page") 
+var currentQuestion = 0
+var startPage = document.querySelector(".start-page")
 var questionPage = document.querySelector(".question-page")
 var scorePage = document.querySelector(".score-page")
 var questionTitle = document.querySelector("#title")
@@ -6,10 +7,12 @@ var answerContainer = document.querySelector(".answer-choices")
 var finalScore = document.querySelector("#final-score")
 var submitButton = document.querySelector("#submit-name-button")
 var startButton = document.querySelector(".start-quiz")
+var inputBox = document.querySelector("#name")
 
 var currentQuestion = 0;
-var time = 75;
-var timer  
+var time = 60;
+var timer = document.querySelector(".timer")
+var timeSet;
 
 var questions = [
     {
@@ -38,25 +41,96 @@ var questions = [
         correctAnswer: "All of the above",
     },
 ];
-function startQuiz () {
- questionPage.setAttribute ("class" , "hide")
+function startQuiz() {
+    startPage.classList.add("hidden")
+    questionPage.classList.remove("hidden")
+    timer.textContent = time
+    countdown()
+    showQuestion()
+    
+
 }
 
-function showQuestion(q) {
-    var titleDiv = querySelectorAll(".title");
+function countdown() {
+    timeSet = setInterval(function () {
+        time -= 1
+        timer.textContent = time
+        if (time <= 0) {
+            time=0
+            timer.textContent = time
+            endQuiz()
 
-    titleDiv.textContent = q.title;
+        }
+    }, 1000)
+}
 
-    document.querySelectorAll(".choices");
-    alts.forEach(function (element, index) {
-        element.textContent = q.alternative[index];
+
+function endQuiz() {
+    clearInterval(timeSet)
+    questionPage.classList.add("hidden")
+    scorePage.classList.remove("hidden")
+    document.getElementById("final-score").textContent=time
+    submitButton.addEventListener("click" , function(){
+        if (inputBox.value !== ""){
+            var score={
+                score:time, 
+                name:inputBox.value
+            }
+            var highScores=JSON.parse(localStorage.getItem("scores")) || []
+            highScores.push(score) 
+            localStorage.setItem("scores" , JSON.stringify(highScores))
+        }
+    })
+    //*display current or saved scores and to capture the initials of the latest score. Hide show question cards aswell//
+}
+
+//*check answer function check if answer is correct or incorrect. If correct tally up the next question in the array. Check if the question is the last in the array if so invoke end quiz. Append next question in the array// 
+
+function showQuestion() {
+    var titleDiv = document.querySelector(".title");
+
+    titleDiv.textContent = questions[currentQuestion].title;
+
+    document.querySelector(".answer-choices").innerHTML = ""
+    questions[currentQuestion].choices.forEach(function (choice, index) {
+        var choiceBtn = document.createElement("button");
+        choiceBtn.textContent = choice
+        choiceBtn.setAttribute("value", choice)
+        choiceBtn.onclick = checkAnswer
+        document.querySelector(".answer-choices").append(choiceBtn)
+
     });
 }
-console.log("startButton" , startButton)
-startButton.addEventListener("click" ,function() {
-    console.log("chicken")
-    
+function checkAnswer() {
+    if (this.value !== questions[currentQuestion].correctAnswer) {
+        time -= 15
+        timer.textContent = time
+        if (time <= 0) {
+            time = 0
+            timer.textContent = time
+            endQuiz()
+        }
+    }
+    else {
+        //* correct feedback//
+    }
+    currentQuestion++
+    if (currentQuestion === questions.length) {
+        endQuiz()
+    }
+    else {
+        showQuestion()
+    }
+
 }
+
+
+function submitButton()
+
+
+console.log("startButton", startButton)
+
+startButton.addEventListener("click", startQuiz);
 
 
 
